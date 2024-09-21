17db12dc491b28d2b65fc4f37808ad7be4c8d84a
Shreyas Damase <113159691+ShreyasDamase@users.noreply.github.com>
2024-09-21 16:01:43 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,10 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) {
-    return next(new ErrorHandler("User not Authenticated!", 400));
-  }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  req.user = await User.findById(decoded.id);
+  // if (!token) {
+  //   return next(new ErrorHandler("User not Authenticated!", 400));
+  // }
+  // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  // req.user = await User.findById(decoded.id);
   next();
 });
